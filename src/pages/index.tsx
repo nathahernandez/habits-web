81c350b8bf81a948b62f164498a85fbf93935e60
Nathã Hernandez <nathahcorrea@gmail.com>
2023-01-24 10:27:57 -0300

    feat: new eslint config changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
-import Habit from '@/components/Habit'
-import Head from 'next/head'
+import Habits from "@/components/Habits"
+import Head from "next/head"
 
 export default function Home() {
 	return (
-		<>
+		<div className="w-screen h-screen flex justify-center items-center">
 			<Head>
 				<title>Habit</title>
 				<meta name="description" content="Application created to track habits" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Habit />
-		</>
+
+			<Habits />
+		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-		'./src/**/*.{js,ts,jsx,tsx}'
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./src/**/*.{js,ts,jsx,tsx}"
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				BlackCustomColor: "#1E1E1E"
+			},
+			gridTemplateRows: {
+				7: "repeat(7, minmax(0, 1fr))"
+			}
+		},
 	},
 	plugins: [],
 }
